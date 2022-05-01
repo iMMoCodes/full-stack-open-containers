ad4d87f0e06f2192b7b2d7d14b3d210d35191ef0
iMMoCodes <matti-makela@hotmail.com>
2022-05-01 17:36:47 +0300

    Tehtävä 12.10

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -5,15 +5,24 @@ const redis = require('../redis')
 const configs = require('../util/config')
 
 let visits = 0
+let createdTodos = 0
 
 /* GET index data. */
 router.get('/', async (req, res) => {
   visits++
-
   res.send({
     ...configs,
     visits,
   })
+})
+
+router.get('/statistics', async (req, res) => {
+  const added_todos = await redis.getAsync('added_todos')
+  if (!added_todos) {
+    redis.setAsync('added_todos', JSON.stringify(createdTodos))
+  } else {
+    res.json({ added_todos: added_todos })
+  }
 })
 
 module.exports = router
--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { Todo } = require('../mongo')
 const router = express.Router()
+const redis = require('../redis')
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
@@ -14,6 +15,8 @@ router.post('/', async (req, res) => {
     text: req.body.text,
     done: false,
   })
+  let added_todos = await redis.getAsync('added_todos')
+  await redis.setAsync('added_todos', JSON.stringify(parseInt(added_todos) + 1))
   res.send(todo)
 })
 
